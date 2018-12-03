2a7da1cc6c90b84024c9fec8e441a56d264f224f
Chen <chenzhu@cs.umd.edu>
2018-12-03 16:28:31 -0500

    sync

--- draw_influence_LR.py
+++ draw_influence_LR.py
@@ -7,27 +7,34 @@ import pickle
 
 # code for plotting total influences computed by SVMs
 
-train_idx = np.load('/scratch0/GoGradients/code/svm_figures/mnist_1_7/train_most_confusing_idxes_C1.npy')[:30]
-hinge_losses = 1 - np.load('/scratch0/GoGradients/code/svm_figures/mnist_1_7/train_margins_C1.npy')
-hinge_losses = (hinge_losses>0) * hinge_losses
-res_dir = './LR_influence/mnist'
-hinge_scale = 100
-figure_title = "Influence vs Hinge Loss\n(on MNIST, influence computed with Logistic Regression)"
-
-# train_idx = np.load('/scratch0/GoGradients/code/svm_figures/train_most_confusing_idxes_C1.npy')[:30]
-# hinge_losses = 1 - np.load('/scratch0/GoGradients/code/svm_figures/train_margins_C1.npy')
+# train_idx = np.load('/scratch0/GoGradients/code/svm_figures/mnist_1_7/train_most_confusing_idxes_C1.npy')[:30]
+# hinge_losses = 1 - np.load('/scratch0/GoGradients/code/svm_figures/mnist_1_7/train_margins_C1.npy')
 # hinge_losses = (hinge_losses>0) * hinge_losses
-# res_dir = './LR_influence/adult'
+# res_dir = './LR_influence/mnist'
 # hinge_scale = 100
-# figure_title = "Influence vs Hinge Loss\n(on Adult, influence computed with Logistic Regression)"
+# figure_title = "Influence vs {}\n(on MNIST, influence computed with Logistic Regression)"
 
-influence_list, hinge_list = [], []
-for idx in train_idx:
-    datadict = pickle.load(open(os.path.join(res_dir, 'influence_{}.pkl'.format(idx))))
+train_idx = np.load('/scratch0/GoGradients/code/svm_figures/train_most_confusing_idxes_C1.npy')[:30]
+hinge_losses = 1 - np.load('/scratch0/GoGradients/code/svm_figures/train_margins_C1.npy')
+hinge_losses = (hinge_losses>0) * hinge_losses
+res_dir = './LR_influence/adult'
+hinge_scale = 100
+figure_title = "Influence vs {}\n(on Adult, influence computed with Logistic Regression)"
+
+influence_list, hinge_list, gt_list = [], [], []
+
+def load_data(datadict):
     total_inf = datadict['total_inf']
     hinge = hinge_losses[idx]
     influence_list.append(total_inf)
     hinge_list.append(hinge)
+
+    gt = np.sum(np.abs(datadict['ground_truth']))
+    gt_list.append(gt)
+
+for idx in train_idx:
+    datadict = pickle.load(open(os.path.join(res_dir, 'influence_{}.pkl'.format(idx))))
+    load_data(datadict)
 
 # load the random samples
 file_list = os.listdir(res_dir)
@@ -40,10 +47,7 @@ for fname in file_list:
         continue
     if idx not in train_idx:
         datadict = pickle.load(open(os.path.join(res_dir, fname)))
-        total_inf = datadict['total_inf']
-        hinge = hinge_losses[idx]
-        influence_list.append(total_inf)
-        hinge_list.append(hinge)
+        load_data(datadict)
 
 # plot it
 plt.figure()
@@ -52,5 +56,14 @@ plt.scatter(hinge_list[:len(train_idx)], influence_list[:len(train_idx)], c=[1,0
 plt.legend(["Randomly Selected Points", "Largest Hinge Points"])
 plt.xlabel("Hinge Loss")
 plt.ylabel("Estimated Influence")
-plt.title(figure_title)
+plt.title(figure_title.format("Hinge Loss"))
 plt.savefig(os.path.join(res_dir, "influence_vs_hinge.png"))
+
+plt.figure()
+plt.scatter(gt_list[len(train_idx):], influence_list[len(train_idx):], c=[0,0,1,0.3])
+plt.scatter(gt_list[:len(train_idx)], influence_list[:len(train_idx)], c=[1,0,0,0.3])
+plt.legend(["Randomly Selected Points", "Largest Hinge Points"])
+plt.xlabel("Ground Truth")
+plt.ylabel("Estimated Influence")
+plt.title(figure_title.format("Ground Truth"))
+plt.savefig(os.path.join(res_dir, "influence_vs_gt.png"))
